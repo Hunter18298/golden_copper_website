65199e2c2c40f4f03d0468943c56f00fdad7d516
Hunter18298 <aland.goran182@gmail.com>
2024-05-02 08:46:38 +0300

    eqw

--- app/navbar.js
+++ app/navbar.js
@@ -1,34 +1,35 @@
 import { useState } from 'react';
 import Link from 'next/link';
-import { MenuIcon, XIcon } from '@heroicons/react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 export default function NavbarCopper() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     return (
-        <nav className="bg-blue-500 text-white p-5">
-            <div className="container mx-auto flex justify-between items-center">
-                <Link className="text-lg font-bold" href="/">
-                    Logo
+        <nav className="bg-gradient-to-r from-yellow-300 to-yellow-500 text-white p-5">
+            <div className="container mx-auto flex justify-between items-center  ">
+                <Link className="text-lg text-black font-bold" href="/">
+                    Golden Copper
                 </Link>
                 <div className="md:hidden">
                     <button onClick={() => setIsMenuOpen(!isMenuOpen)}>
                         {isMenuOpen ? (
-                            <XIcon className="h-8 w-8" />
+                            <FontAwesomeIcon icon={faTimes} className="h-8 w-8" />
                         ) : (
-                            <MenuIcon className="h-8 w-8" />
+                            <FontAwesomeIcon icon={faBars} className="h-8 w-8" />
                         )}
                     </button>
                 </div>
-                <div className={`absolute bg-blue-500 w-full left-0 top-16 md:static md:flex md:items-center md:w-auto ${isMenuOpen ? 'block' : 'hidden'}`}>
+                <div className={`absolute  w-full left-0 top-16 md:static md:flex md:items-center md:w-auto ${isMenuOpen ? 'block' : 'hidden'}`}>
                     <ul className="flex flex-col md:flex-row md:space-x-8 mt-5 md:mt-0">
+                        <li><Link className="block py-2" href="#">Home</Link></li>
+                        <li><Link className="block py-2" href="#">Contact</Link></li>
                         <li><Link className="block py-2" href="#">About</Link></li>
-                        <li><Link className="block py-2" href="#">Services</Link></li>
-                        <li><Link className="block py-2" href="/#">Contact</Link></li>
+
                     </ul>
                 </div>
             </div>
         </nav>
     );
 };
-
